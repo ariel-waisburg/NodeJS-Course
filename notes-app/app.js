7d9c690d0faf7901cadd1886ba351969c8f7949f
ariel-waisburg <arielwaisburg@gmail.com>
2021-04-22 08:22:31 -0300

    getting info from users

--- notes-app/app.js
+++ notes-app/app.js
@@ -9,10 +9,25 @@
 const chalk = require('chalk')
 const notes = require('./notes')
 
-const printNotes = notes()
+// const printNotes = notes()
 
-console.log(printNotes)
+// console.log(printNotes)
 
-console.log(chalk.green.bold.inverse('Success!'));
+// console.log(chalk.green.bold.inverse('Success!'));
 
-// console.log(validator.isURL('http://ariel.io'))
+// // console.log(validator.isURL('http://ariel.io'))
+
+const command = process.argv[2]
+
+console.log(process.argv[3])
+
+if (command === 'add') {
+  console.log('adding a note!')
+} else if (command === 'remove') {
+  console.log('deleting a note')
+} else if (command === 'update') {
+  console.log('updating a note')
+}
+else {
+  console.log('listing notes...')
+}
