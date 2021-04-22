5a631bf6d890d2827d040e3dd717eab48caf78f6
ariel-waisburg <arielwaisburg@gmail.com>
2021-04-22 08:49:06 -0300

    argument parsing with yargs part 2

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,5 @@
 const chalk = require('chalk')
+const { argv } = require('yargs')
 const yargs = require('yargs')
 const notes = require('./notes')
 
@@ -6,8 +7,21 @@ const notes = require('./notes')
 yargs.command({
   command: 'add',
   describe: 'Add a new note',
-  handler: function () {
-    console.log('Adding a new note!')
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note description',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function (argv) {
+    console.log('Title: ' + argv.title)
+    console.log('Body: ' + argv.body)
   }
 })
 
@@ -38,4 +52,4 @@ yargs.command({
   }
 })
 
-console.log(yargs.argv)
+yargs.parse()
