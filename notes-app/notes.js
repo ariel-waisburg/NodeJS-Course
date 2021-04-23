3caddfc4c9e6d628c7f5384b8b9ca21bf43c6be5
ariel-waisburg <arielwaisburg@gmail.com>
2021-04-23 08:39:15 -0300

    adding a note

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,42 @@
+const fs = require('fs')
+
 const getNotes = function () {
-return 'These are your pending notes...'
+  return 'These are your notes...'
 }
 
-module.exports = getNotes
+const loadNotes = function () {
+  try {
+    const dataJSON = fs.readFileSync('notes.json').toString()
+    return JSON.parse(dataJSON)
+  } catch (e) {
+    return []
+  }
+}
+
+const saveNotes = function (notes) {
+  const dataJSON = JSON.stringify(notes)
+  fs.writeFileSync('notes.json', dataJSON)
+}
+
+const addNote = function (title, body) {
+  const notes = loadNotes()
+  const duplicateNotes = notes.filter(function (note) {
+    return note.title === title
+  })
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body
+    })
+    saveNotes(notes)
+    console.log('New note created')
+  } else {
+    console.log('Note title taken!')
+  }
+}
+
+module.exports = {
+  getNotes: getNotes,
+  addNote: addNote
+}
